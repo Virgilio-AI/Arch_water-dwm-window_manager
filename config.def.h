b6dd0497f5222601de31527614983c6f2dc5d805
Virgilio <virgiliomurilloochoa1@gmail.com>
2021-07-24 23:48:29 -0500

    patched with alpha for transparency

--- config.def.h
+++ config.def.h
@@ -14,12 +14,23 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const unsigned int baralpha = 0xd0;
+static const unsigned int borderalpha = OPAQUE;
+
 static const char col_red[]         = "#FF0000";
 static const char col_purple[]      = "#8A619E";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_purple,  col_red  },
+};
+
+
+
+static const unsigned int alphas[][3]      = {
+	/*               fg      bg        border     */
+	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
