1931359ec98717da16b7d2aec24cd0abfb22cc21
Virgilio <virgiliomurilloochoa1@gmail.com>
2021-07-26 01:00:58 -0500

    added binding for pass(password manager)

--- config.h
+++ config.h
@@ -79,6 +79,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 //custom commands
+static const char *open_password_manager[] = { "passmenu", NULL };
 static const char *open_notes[] = { "sh","open_notes.sh","&","disown",";","exit", NULL };
 static const char *open_ranger[] = { TERMINAL,"open_cfiles.sh", NULL };
 static const char *open_qbrowser[] = { "qutebrowser", NULL };
@@ -96,6 +97,8 @@ static Key keys[] = {
 	{MODKEY,           XK_n,                    spawn,          {.v = open_notes } },
 	{MODKEY,           XK_v,                    spawn,          {.v = show_clipboard } },
 	{MODKEY,           XK_space,                spawn,          {.v = change_keyboard_layout } },
+	{MODKEY|ShiftMask, XK_p,                    spawn,          {.v = open_password_manager } },
+
 	//print screen
 	{0,		   XK_Print,                spawn,          {.v = print_screen } },
 
